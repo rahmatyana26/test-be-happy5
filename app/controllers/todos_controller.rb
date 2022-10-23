5c50feb7ea1d0e78b944fb944b8165b9ab408da4
rahmatyana26 <90589127+rahmatyana26@users.noreply.github.com>
2022-10-23 14:31:05 +0700

    Authentication Controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,15 +2,16 @@ class TodosController < ApplicationController
     before_action :set_todo, only: [:show, :update, :destroy]
   
     # GET /todos
-    def index
-      @todos = Todo.all
-      json_response(@todos)
-    end
+  def index
+    # get current user todos
+    @todos = current_user.todos
+    json_response(@todos)
+  end
   
     # POST /todos
-    # POST /todos
   def create
-    @todo = Todo.create!(todo_params)
+    # create todos belonging to current user
+    @todo = current_user.todos.create!(todo_params)
     json_response(@todo, :created)
   end
   
@@ -33,9 +34,9 @@ class TodosController < ApplicationController
   
     private
   
+    # remove `created_by` from list of permitted parameters
     def todo_params
-      # whitelist params
-      params.permit(:title, :created_by)
+        params.permit(:title)
     end
   
     def set_todo
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,7 @@
-# spec/factories/users.rb
 FactoryBot.define do
     factory :user do
-      name { Faker::Name.name }
-      email 'foo@bar.com'
-      password 'foobar'
+        name { Faker::Name.name}
+        email { 'foo@bar.com' }
+        password { 'foobar' }
     end
-  end
+end
